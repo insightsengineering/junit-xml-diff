38fd2558b615a60a84abb37dbc34722cbc100115
Franciszek Walkowiak <franciszek.walkowiak@roche.com>
2023-01-30 17:33:38 +0100

    Test case report

--- main.go
+++ main.go
@@ -21,19 +21,21 @@ type TestSuitesXML struct {
 }
 
 type TestSuite struct {
-	XMLName  xml.Name `xml:"testsuite"`
-	Time     string   `xml:"time,attr"`
-	Name     string   `xml:"name,attr"`
-	Tests    int      `xml:"tests,attr"`
-	Skipped  int      `xml:"skipped,attr"`
-	Failures int      `xml:"failures,attr"`
-	Errors   int      `xml:"errors,attr"`
+	XMLName   xml.Name   `xml:"testsuite"`
+	Time      string     `xml:"time,attr"`
+	Name      string     `xml:"name,attr"`
+	Tests     int        `xml:"tests,attr"`
+	Skipped   int        `xml:"skipped,attr"`
+	Failures  int        `xml:"failures,attr"`
+	Errors    int        `xml:"errors,attr"`
+	TestCases []TestCase `xml:"testcase"`
 }
 
 type TestCase struct {
-	XMLName xml.Name `xml:"testcase"`
-	Time    string   `xml:"time,attr"`
-	Name    string   `xml:"name,attr"`
+	XMLName   xml.Name `xml:"testcase"`
+	Time      string   `xml:"time,attr"`
+	Name      string   `xml:"name,attr"`
+	ClassName string   `xml:"classname,attr"`
 }
 
 type TestSuiteDiff struct {
@@ -45,7 +47,27 @@ type TestSuiteDiff struct {
 	ErrorsDiff   string
 }
 
-func getTestSuitesTime(testSuiteXML TestSuitesXML) (map[string]string, map[string]int,
+type TestCaseTime struct {
+	Time         float32
+	SuiteName    string
+	ClassName    string
+	TestCaseName string
+}
+
+type TestCaseDiff struct {
+	TestCaseStatus string
+	TimeDiff       string
+	TestCaseName   string
+	ClassName      string
+	SuiteName      string
+}
+
+type TestReport struct {
+	SuiteDiff map[string]TestSuiteDiff
+	CaseDiff  map[string]TestCaseDiff
+}
+
+func getTestSuites(testSuiteXML TestSuitesXML) (map[string]string, map[string]int,
 	map[string]int, map[string]int, map[string]int) {
 	testSuiteTimes := make(map[string]string)
 	testSuiteTests := make(map[string]int)
@@ -62,14 +84,33 @@ func getTestSuitesTime(testSuiteXML TestSuitesXML) (map[string]string, map[strin
 	return testSuiteTimes, testSuiteTests, testSuiteSkipped, testSuiteFailures, testSuiteErrors
 }
 
-func getTestCaseTime(testSuiteXML TestSuitesXML) (map[string]string, map[string]string) {
-	testSuiteTimes := make(map[string]string)
-	testSuiteTestCases := make(map[string]string)
+func getTestCases(testSuiteXML TestSuitesXML) map[string]TestCaseTime {
+	// Test case is identified by: testsuitename:testcaseclassname:testcasename
+	testCaseTimes := make(map[string]TestCaseTime)
 	for _, v := range testSuiteXML.TestSuites {
-		testSuiteTimes[v.Name] = v.Time
-		testSuiteTestCases[v.Name] = v.Tests
+		testSuiteName := v.Name
+		for _, testCase := range v.TestCases {
+			testCaseTime, err := strconv.ParseFloat(testCase.Time, 32)
+			checkError(err)
+			testCaseID := testSuiteName + ":" + testCase.ClassName + ":" + testCase.Name
+			// It may happen that there are multiple test cases with the same class name and name inside a test suite.
+			// In that case, times of such cases are added to each other.
+			testCaseTimeEntry, ok := testCaseTimes[testCaseID]
+			if ok {
+				testCaseTimeEntry.Time += float32(testCaseTime)
+				testCaseTimes[testCaseID] = testCaseTimeEntry
+			} else {
+				testCaseTimes[testCaseID] = TestCaseTime{
+					float32(testCaseTime),
+					testSuiteName,
+					testCase.ClassName,
+					testCase.Name,
+				}
+
+			}
+		}
 	}
-	return testSuiteTimes, testSuiteTestCases
+	return testCaseTimes
 }
 
 func formatFloat(number float32) string {
@@ -88,21 +129,61 @@ func formatInt(number int) string {
 	return plusSign + strconv.Itoa(number)
 }
 
-func compareTestSuites(testSuiteOld []byte, testSuiteNew []byte) map[string]TestSuiteDiff {
-	var testSuiteNewXML TestSuitesXML
-	var testSuiteOldXML TestSuitesXML
-	err := xml.Unmarshal(testSuiteNew, &testSuiteNewXML)
-	checkError(err)
-	err = xml.Unmarshal(testSuiteOld, &testSuiteOldXML)
-	checkError(err)
+func compareTestCases(testSuiteOld TestSuitesXML, testSuiteNew TestSuitesXML) map[string]TestCaseDiff {
+	testCaseTimesOld := getTestCases(testSuiteOld)
+	testCaseTimesNew := getTestCases(testSuiteNew)
+	testCaseTimeDiff := make(map[string]TestCaseDiff)
+	// Iterate through new test cases times.
+	for k := range testCaseTimesNew {
+		// Check if the test case existed previously.
+		_, ok := testCaseTimesOld[k]
+		if ok {
+			// Test case exists both in old and new XML.
+			testCaseTimeDiff[k] = TestCaseDiff{
+				"",
+				formatFloat(testCaseTimesNew[k].Time - testCaseTimesOld[k].Time),
+				testCaseTimesNew[k].TestCaseName,
+				testCaseTimesNew[k].ClassName,
+				testCaseTimesNew[k].SuiteName,
+			}
+		} else {
+			// Test case exists only in new XML.
+			testCaseTimeDiff[k] = TestCaseDiff{
+				"➕",
+				formatFloat(testCaseTimesNew[k].Time),
+				testCaseTimesNew[k].TestCaseName,
+				testCaseTimesNew[k].ClassName,
+				testCaseTimesNew[k].SuiteName,
+			}
+		}
+	}
+	// Iterate through old test cases times.
+	for k := range testCaseTimesOld {
+		_, ok := testCaseTimesNew[k]
+		if !ok {
+			// Test case exists only in old XML.
+			testCaseTimeDiff[k] = TestCaseDiff{
+				"➖",
+				formatFloat(testCaseTimesOld[k].Time),
+				testCaseTimesOld[k].TestCaseName,
+				testCaseTimesOld[k].ClassName,
+				testCaseTimesOld[k].SuiteName,
+			}
+		}
+	}
+	return testCaseTimeDiff
+}
+
+func compareTestSuites(testSuiteOld TestSuitesXML, testSuiteNew TestSuitesXML) map[string]TestSuiteDiff {
+
 	testSuiteTimesOld, testSuiteTestsOld, testSuiteSkippedOld,
-		testSuiteFailuresOld, testSuiteErrorsOld := getTestSuitesTime(testSuiteOldXML)
+		testSuiteFailuresOld, testSuiteErrorsOld := getTestSuites(testSuiteOld)
 	testSuiteTimesNew, testSuiteTestsNew, testSuiteSkippedNew,
-		testSuiteFailuresNew, testSuiteErrorsNew := getTestSuitesTime(testSuiteNewXML)
+		testSuiteFailuresNew, testSuiteErrorsNew := getTestSuites(testSuiteNew)
 	testSuiteDiff := make(map[string]TestSuiteDiff)
 
 	// Iterate through test suites in new XML.
-	for _, v := range testSuiteNewXML.TestSuites {
+	for _, v := range testSuiteNew.TestSuites {
 		testSuiteName := v.Name
 		newTime := testSuiteTimesNew[testSuiteName]
 		newTimeFloat, err := strconv.ParseFloat(newTime, 32)
@@ -112,7 +193,7 @@ func compareTestSuites(testSuiteOld []byte, testSuiteNew []byte) map[string]Test
 		newFailures := testSuiteFailuresNew[testSuiteName]
 		newErrors := testSuiteErrorsNew[testSuiteName]
 		// Check if the test suite existed previously.
-		// Keys in all maps returned by getTestSuitesTimes are the same,
+		// Keys in all maps returned by getTestSuites are the same,
 		// so it is okay to iterate through any of these maps.
 		_, ok := testSuiteTimesOld[testSuiteName]
 		if ok {
@@ -141,7 +222,7 @@ func compareTestSuites(testSuiteOld []byte, testSuiteNew []byte) map[string]Test
 		}
 	}
 	// Iterate through test suites in old XML.
-	for _, v := range testSuiteOldXML.TestSuites {
+	for _, v := range testSuiteOld.TestSuites {
 		testSuiteName := v.Name
 		_, ok := testSuiteTimesNew[v.Name]
 		if !ok {
@@ -162,16 +243,28 @@ func compareTestSuites(testSuiteOld []byte, testSuiteNew []byte) map[string]Test
 }
 
 const mdtemplate = `
-|Test Name|Status|±Time|±Tests|±Skipped|±Failures|±Errors|
-|----|------|----|-----|-------|--------|------|
-{{- range $key, $value := .}}
-|{{ $key }}|{{ $value.SuiteStatus }}|{{ $value.TimeDiff }}|{{ $value.TestsDiff }}|{{ $value.SkippedDiff }}|{{ $value.FailuresDiff }}|{{ $value.ErrorsDiff }}|
+|Test Suite|Status|±Time|±Tests|±Skipped|±Failures|±Errors|
+|----|----|----|-----|-------|--------|------|
+{{- range $key, $value := .SuiteDiff }}
+|{{ $key }}|{{ .SuiteStatus }}|{{ .TimeDiff }}|{{ .TestsDiff }}|{{ .SkippedDiff }}|{{ .FailuresDiff }}|{{ .ErrorsDiff }}|
 {{- end}}
+
+<details>
+  <summary>Click me</summary>
+
+|Test Suite|Class|Test Name|Status|±Time|
+|----|----|----|----|----|
+{{- range $key, $value := .CaseDiff }}
+|{{ .SuiteName }}|{{ .ClassName }}|{{ .TestCaseName }}|{{ .TestCaseStatus }}|{{ .TimeDiff }}|
+{{- end}}
+</details>
 `
+
+// TODO is class name relevant? It is mostly the same as test suite name
 
 func main() {
 	if len(os.Args) < 3 {
-		fmt.Printf("Usage: ./%s <old-xml-file-name> <new-xml-file-name>\n", os.Args[0])
+		fmt.Printf("Usage: %s <old-xml-file-name> <new-xml-file-name>\n", os.Args[0])
 		os.Exit(1)
 	}
 	xmlFileOld, err := os.Open(os.Args[1])
@@ -185,10 +278,22 @@ func main() {
 	byteValueNew, err := io.ReadAll(xmlFileNew)
 	checkError(err)
 
-	testSuiteDiff := compareTestSuites(byteValueOld, byteValueNew)
+	var testSuiteNewXML TestSuitesXML
+	var testSuiteOldXML TestSuitesXML
+	err = xml.Unmarshal(byteValueNew, &testSuiteNewXML)
+	checkError(err)
+	err = xml.Unmarshal(byteValueOld, &testSuiteOldXML)
+	checkError(err)
+
+	testSuiteDiff := compareTestSuites(testSuiteOldXML, testSuiteNewXML)
+	testCasesDiff := compareTestCases(testSuiteOldXML, testSuiteNewXML)
+
+	var testReport TestReport
+	testReport.SuiteDiff = testSuiteDiff
+	testReport.CaseDiff = testCasesDiff
 
 	tmpl, err := template.New("md").Parse(mdtemplate)
 	checkError(err)
-	err = tmpl.Execute(os.Stdout, testSuiteDiff)
+	err = tmpl.Execute(os.Stdout, testReport)
 	checkError(err)
 }
